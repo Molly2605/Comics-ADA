6ccba154e7688fe9f32122891684b1fbe0b4bed2
FranciscoB <franciscob@pagotic.com>
2024-10-23 14:08:32 -0300

    Limpieza de codigo

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const urlApi = "http://gateway.marvel.com";
 const urlPersonajes = "/v1/public/characters";
 const publicKey = "74f0cbbf5b058f085a7c652c6030d2a7";
 const ts = "frutilla";
-const hash ="3eefd85d7a0a3bdaca81076858f93324"; /*ts + clave privada + clave publica */
+const hash = "3eefd85d7a0a3bdaca81076858f93324"; /*ts + clave privada + clave publica */
 const paramAutenticacion = `?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-
+/**
 fetch(urlApi + urlPersonajes + paramAutenticacion, {
   method: "GET",
   headers: {
@@ -14,29 +14,53 @@ fetch(urlApi + urlPersonajes + paramAutenticacion, {
   .then((response) => response.json())
   .then((data) => console.log(data))
   .catch((error) => console.error(error));
-
+ */
 
 
 const personajesContainer = document.getElementById('personajes-container');
 const comicsContainer = document.getElementById('comic-container');
+const comicsSection = document.getElementById('comics-section');
+const personajeSection = document.getElementById('personaje-section');
+
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('input-search');
+const searchType = document.getElementById('search-type');
+const searchSort = document.getElementById('search-sort');
+
+let firstPage = document.getElementById('btn-first-page');
+let prevPage = document.getElementById('btn-prev-page');
+let nextPage = document.getElementById('btn-next-page');
+let lastPage = document.getElementById('btn-last-page');
+let firstPageBottom = document.getElementById('btn-first-page-bottom');
+let prevPageBottom = document.getElementById('btn-prev-page-bottom');
+let nextPageBottom = document.getElementById('btn-next-page-bottom');
+let lastPageBottom = document.getElementById('btn-last-page-bottom');
+let currentPageDisplay = document.getElementById('current-page');
+let currentPageBottomDisplay = document.getElementById('current-page-bottom');
 
 
-/*NAVEGACION COMICS - PERSONAJES*/
-function mostrarPantallaComics(){
+/*NAVEGACION COMICS*/
+function mostrarPantallaComics() {
+  console.log("Cambiando a pantalla comics");
   comicsContainer.classList.remove('hidden');
   personajesContainer.classList.add('hidden');
+  comicsSection.classList.remove('hidden');
+  personajeSection.classList.add('hidden');
 };
 
-
-function mostrarPantallaPersonajes(){
-comicsContainer.classList.add('hidden');
-personajesContainer.classList.remove('hidden');
+/*NAVEGACION  PERSONAJES*/
+function mostrarPantallaPersonajes() {
+  console.log("Cambiando a pantalla personajes");
+  comicsContainer.classList.add('hidden');
+  personajesContainer.classList.remove('hidden');
+  comicsSection.classList.add('hidden');
+  personajeSection.classList.remove('hidden');
 }
-
 
 
 /*Consigo Comics*/
 const getComics = () => {
+  console.log("consigo comics");
   const urlComics = `${urlApi}/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=20`;
 
   fetch(urlComics, {
@@ -54,9 +78,8 @@ const getComics = () => {
 };
 
 const displayComics = (comics) => {
+  console.log("display de comics");
   comicsContainer.innerHTML = '';
-  
-
   comics.forEach(comic => {
     const comicHTML = `
     <div class="rounded-lg w-60 overflow-hidden text-black transition-transform duration-300 ease-in-out hover:text-red-500 transform hover:scale-105">
@@ -74,6 +97,7 @@ getComics();
 
 /*Consigo Personajes*/
 const getPersonajes = () => {
+  console.log("consigo personajes");
   const urlPersonajes = `${urlApi}/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=20`;
 
   fetch(urlPersonajes, {
@@ -107,72 +131,40 @@ getPersonajes();
 
 
 
-/*Busqueda por nombre de comics
-searchButton.addEventListener('click', () => {
-  const nombrePersonaje = searchInput.value.trim();
-  if (nombrePersonaje) {
-    buscarPersonajePorNombre(nombrePersonaje);
+
+
+function fetchPersonajes() {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const personajes = data.data.results;
+      personajesContainer.innerHTML = ''; // Limpia el contenedor
+      personajes.forEach(personaje => {
+        const personajeCard = `
+          <div class="personaje-card">
+            <h3>${personaje.name}</h3>
+            <img src="${personaje.thumbnail.path}.${personaje.thumbnail.extension}" alt="${personaje.name}" />
+          </div>
+        `;
+        personajesContainer.innerHTML += personajeCard; // Agrega cada personaje al contenedor
+      });
+    })
+    .catch(error => console.error('Error fetching personajes:', error));
+}
+
+function handleSearchTypeChange(type) {
+  if (type === 'characters') {
+    mostrarPantallaPersonajes(); // Llama a la función para mostrar personajes
   } else {
-    alert('Por favor, ingresa un nombre de personaje');
+    mostrarPantallaComics(); // Llama a la función para mostrar cómics
   }
-});
-*/
+}
 
 
-/*const buscarPersonajePorNombre = (nombre) => {
-  const urlPersonaje = `${urlApi}/v1/public/characters?name=${nombre}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-
-  fetch(urlPersonaje, {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json",
-    },
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      if (data.data.results.length > 0) {
-        const personaje = data.data.results[0];
-        const personajeId = personaje.id;
-        obtenerComicsPorPersonaje(personajeId);
-      } else {
-        alert('Personaje no encontrado');
-      }
-    })
-    .catch(error => console.error('Error buscando personaje:', error));
-};*/
-
-
-/*const obtenerComicsPorPersonaje = (personajeId) => {
-  const urlComics = `${urlApi}/v1/public/characters/${personajeId}/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=20`;
-
-  fetch(urlComics, {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json",
-    },
-  })
-    .then(response => response.json())
-    .then(data => {
-      const comics = data.data.results;
-      displayComics(comics);
-    })
-    .catch(error => console.error('Error buscando cómics:', error));
-};
-
-
-
-Ejemplo de uso de la función, uttilice a Spider-Man 
+/*Ejemplo de uso de la función, uttilice a Spider-Man 
 const characterId = '1009610';
 getComicsByCharacter(characterId);
  */
-
-
-/**/
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('input-search');
-const searchType = document.getElementById('search-type');
-const searchSort = document.getElementById('search-sort');
 
 searchButton.addEventListener('click', () => {
   const query = searchInput.value.trim();
@@ -193,6 +185,7 @@ searchButton.addEventListener('click', () => {
 
 /*Comics por nombre*/
 const buscarComicsPorNombre = (nombre, order) => {
+  console.log("buscar comics por nombre");
   let urlComics = `${urlApi}/v1/public/comics?titleStartsWith=${nombre}&ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=20`;
 
   fetch(urlComics, {
@@ -204,17 +197,30 @@ const buscarComicsPorNombre = (nombre, order) => {
     .then(response => response.json())
     .then(data => {
       let comics = data.data.results;
-      
-      // Ordenar los cómics
+
       comics = ordenarResultados(comics, order);
-      
-      displayComics(comics);  // Mostrar cómics en pantalla
+
+      displayComics(comics);
     })
     .catch(error => console.error('Error buscando cómics:', error));
 };
 
+// Función para ordenar los resultados
+const ordenarResultados = (comics, order) => {
+  console.log("ordenar resultados");
+  if (order === 'title') {
+    return comics.sort((a, b) => a.title.localeCompare(b.title));
+  } else if (order === 'modified') {
+    return comics.sort((a, b) => new Date(b.modified) - new Date(a.modified));
+  }
+
+  return comics;
+};
+
+
 /*Personajes por nombre*/
 const buscarPersonajesPorNombre = (nombre, order) => {
+  console.log("buscar personajes por nombre");
   let urlPersonajes = `${urlApi}/v1/public/characters?nameStartsWith=${nombre}&ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=20`;
 
   fetch(urlPersonajes, {
@@ -226,33 +232,30 @@ const buscarPersonajesPorNombre = (nombre, order) => {
     .then(response => response.json())
     .then(data => {
       let personajes = data.data.results;
-
-      // Ordenar personajes
       personajes = ordenarResultados(personajes, order);
-
-      displayPersonajes(personajes);  // Mostrar personajes en pantalla
+      displayPersonajes(personajes);
     })
     .catch(error => console.error('Error buscando personajes:', error));
 };
 
-/*FILTROS mas nuevos, mas viejos, a-z, z-a*/
-const obtenerComics = (orden) => {
-  let orderBy = 'title'
 
-  // Establecer el valor de orderBy según la selección del usuario
+
+/*FILTROS mas nuevos, mas viejos, a-z, z-a*/
+const filtroComics = (orden) => {
+  console.log("filtros de los comics");
+  let orderBy = 'title'
   if (orden === 'byOlder') {
-    orderBy = 'focDate'; // Cambia esto si hay un valor específico para más viejo
+    orderBy = 'focDate';
   } else if (orden === 'byNewer') {
-    orderBy = '-focDate'; // Cambia esto si hay un valor específico para más nuevo
+    orderBy = '-focDate';
   } else if (orden === 'a-z') {
     orderBy = 'title';
   } else if (orden === 'z-a') {
     orderBy = '-title';
   }
 
-  // Construir la URL con orderBy
-  const urlComics = `${urlApi}/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}&orderBy=${orderBy}&limit=20`;
 
+  const urlComics = `${urlApi}/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}&orderBy=${orderBy}&limit=20`;
   fetch(urlComics)
     .then(response => response.json())
     .then(data => {
@@ -262,19 +265,11 @@ const obtenerComics = (orden) => {
     .catch(error => console.error('Error fetching comics:', error));
 };
 
-const buscarComics = () => {
-  const orden = document.getElementById('search-sort').value; // Obtener valor del filtro
-  obtenerComics(orden); // Pasar el valor a obtenerComics
-};
-
-document.getElementById('search-button').addEventListener('click', buscarComics);
-
-
 
 /*Buttons navegación de páginas*/
 let currentPage = 1;
 let totalPages = 0;
-const limit = 20; 
+const limit = 20;
 
 const fetchComics = async (page) => {
   const offset = (page - 1) * limit;
@@ -285,50 +280,60 @@ const fetchComics = async (page) => {
     if (!response.ok) {
       throw new Error('Error al obtener cómics');
     }
-    
+
     const data = await response.json();
-    totalPages = Math.ceil(data.data.total / limit); 
-    displayComics(data.data.results); 
+    totalPages = Math.ceil(data.data.total / limit);
+    displayComics(data.data.results);
     updatePaginationButtons();
   } catch (error) {
     console.error('Error al obtener cómics:', error);
   }
 };
 
+/*let firstPage = false; 
+let prevPage = false;
+let nextPage = false;
+let lastPage = false;
+let firstPageBottom = false;
+let prevPageBottom = false;
+let nextPageBottom = false;
+let lastPageBottom = false;*/
+
 const updatePaginationButtons = () => {
-  document.getElementById('btn-first-page').disabled = currentPage === 1;
-  document.getElementById('btn-prev-page').disabled = currentPage === 1;
-  document.getElementById('btn-next-page').disabled = currentPage === totalPages;
-  document.getElementById('btn-last-page').disabled = currentPage === totalPages;
-  document.getElementById('btn-first-page-bottom').disabled = currentPage === 1;
-  document.getElementById('btn-prev-page-bottom').disabled = currentPage === 1;
-  document.getElementById('btn-next-page-bottom').disabled = currentPage === totalPages;
-  document.getElementById('btn-last-page-bottom').disabled = currentPage === totalPages;
-  
-  document.getElementById('current-page').textContent = `PÁGINA ${currentPage}`;
-  document.getElementById('current-page-bottom').textContent =`PÁGINA ${currentPage}`;
+  firstPage = currentPage === 1;
+  prevPage = currentPage === 1;
+  nextPage = currentPage === totalPages;
+  lastPage = currentPage === totalPages;
+  firstPageBottom = currentPage === 1;
+  prevPageBottom = currentPage === 1;
+  nextPageBottom = currentPage === totalPages;
+  lastPageBottom = currentPage === totalPages;
+
+  currentPageDisplay = `PÁGINA ${currentPage}`;
+  currentPageBottomDisplay = `PÁGINA ${currentPage}`;
 };
 
+
 /*listeners button páginado*/
-document.getElementById('btn-first-page').addEventListener('click', () => {
+firstPage.addEventListener('click', () => {
   currentPage = 1;
   fetchComics(currentPage);
 });
 
-document.getElementById('btn-first-page-bottom').addEventListener('click', () => {
+firstPageBottom.addEventListener('click', () => {
   currentPage = 1;
   fetchComics(currentPage);
 });
 
 
 
-document.getElementById('btn-prev-page').addEventListener('click', () => {
+prevPage.addEventListener('click', () => {
   if (currentPage > 1) {
     currentPage--;
     fetchComics(currentPage);
   }
 });
-document.getElementById('btn-prev-page-bottom').addEventListener('click', () => {
+prevPageBottom.addEventListener('click', () => {
   if (currentPage > 1) {
     currentPage--;
     fetchComics(currentPage);
@@ -337,14 +342,14 @@ document.getElementById('btn-prev-page-bottom').addEventListener('click', () => 
 
 
 
-document.getElementById('btn-next-page').addEventListener('click', () => {
+nextPage.addEventListener('click', () => {
   if (currentPage < totalPages) {
     currentPage++;
     fetchComics(currentPage);
   }
 });
 
-document.getElementById('btn-next-page-bottom').addEventListener('click', () => {
+nextPageBottom.addEventListener('click', () => {
   if (currentPage < totalPages) {
     currentPage++;
     fetchComics(currentPage);
@@ -353,12 +358,12 @@ document.getElementById('btn-next-page-bottom').addEventListener('click', () => 
 
 
 
-document.getElementById('btn-last-page').addEventListener('click', () => {
+lastPage.addEventListener('click', () => {
   currentPage = totalPages;
   fetchComics(currentPage);
 });
 
-document.getElementById('btn-last-page-bottom').addEventListener('click', () => {
+lastPageBottom.addEventListener('click', () => {
   currentPage = totalPages;
   fetchComics(currentPage);
 });
@@ -367,12 +372,10 @@ document.getElementById('btn-last-page-bottom').addEventListener('click', () => 
 
 /*initialize App*/
 const initializeApp = async () => {
-  obtenerComics('a-z');
+  console.log("Iniciando app")
+  filtroComics('a-z');
   mostrarPantallaComics();
   await fetchComics(currentPage);
 };
+
 initializeApp();
-
-
-
-
